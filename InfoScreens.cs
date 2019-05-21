ded223036b731f64450547e57e740e47c26dcd4b
Stik <stikdragon@gmail.com>
2019-05-21 23:16:25 +0100

    works now

--- InfoScreens.cs
+++ InfoScreens.cs
@@ -1,5 +1,5 @@
 public Program() {
-    Runtime.UpdateFrequency = UpdateFrequency.Update100;
+    Runtime.UpdateFrequency = UpdateFrequency.Update10;
 }
 
 //
@@ -7,39 +7,93 @@ public Program() {
 //   Create LCD panels with a Custom Data field starting with "stik:lcd-info"
 //   Then the rest of the Custom Data is a configuration string:
 //
-//     stik:lcd-info
-//     Ship Mass: <mass,7>
+//     !stik:lcd-info
+//     Ship Mass: <mass:,7>
 //     
 //     Ores in Cargo:
 //     ---------------
-//         Iron Ore: <IronOre, 7>
-//       Nickel Ore: <NickelOre, 7>
-//         Gold Ore: <GoldOre, 7>
+//         Iron Ore: <item:Ore_Iron, 7>
+//       Nickel Ore: <item:Ore_Nickel, 7>
+//         Gold Ore: <item:Ore_Gold, 7>
 //  
 //     Products:
 //     ----------
-//       Plates:  <SteelPlate, 5>
+//       Plates:  <Component_SteelPlate, 5>
 //       etc...
 //
 //   Give it a few seconds to update after you make a change, it only checks and parses
 //   this string every few seconds, to avoid unnecessary CPU load
 //
+
+
+//
+// Important! You must set this to the name of a cockpit on your grid
+// (It is how the script reads the mass of the ship)
+//
+public static readonly string COCKPIT_NAME = "Cockpit 2";
+
+
+private int updateTimer = 0;
+private readonly int UPDATE_PERIOD = 70; // 700 ticks
+private Dictionary<IMyTextPanel, OutputPanel> panels = new Dictionary<IMyTextPanel, OutputPanel>();
+
 public void Main(string argument, UpdateType updateSource) {
-    IMyShipController gController = GridTerminalSystem.GetBlockWithName("Cockpit 2") as IMyShipController;
-
-    int totalMass = (int)(gController.CalculateShipMass().TotalMass / 1000.0);
-    string s = "Mass: " + totalMass + " T";
-
-    List<IMyTerminalBlock> tmp = new List<IMyTerminalBlock>();
-    GridTerminalSystem.GetBlocksOfType<IMyTextPanel>(tmp);
-    foreach(var b in tmp) {
-        var x = b as IMyTextPanel;
-        if (x.CustomData.Equals("mass"))
-            x.WriteText(s);
-   }    
+    if (updateTimer-- <= 0) {
+        updateTimer = UPDATE_PERIOD;        
+        UpdateScreens(GridTerminalSystem);        
+    }
 }
 
 
+private void UpdateScreens(IMyGridTerminalSystem gts) {
+    //
+    // Look for any changes to LCD configs
+    //
+    List<IMyTerminalBlock> lst = new List<IMyTerminalBlock>();
+    GridTerminalSystem.GetBlocksOfType<IMyTextPanel>(lst);
+    foreach(var b in lst) {
+        var x = b as IMyTextPanel;
+        if (x.CustomData.StartsWith("!stik:lcd-info")) {
+            OutputPanel op;
+            if (!panels.TryGetValue(x, out op)) 
+                panels.Add(x, op = new OutputPanel(x));
+            if (!x.CustomData.Equals(op.Config))
+                UpdatePanelConfig(op);
+        }
+    }
+
+    //
+    // Get list of all cargo, and apply to any screens we have
+    //
+    Dictionary<string, int> cargo = GetAllCargo(GridTerminalSystem);
+    // StringBuilder sb = new StringBuilder();
+    // foreach (var e in cargo) 
+    //     sb.Append(e.Key + "\n");
+    foreach (KeyValuePair<IMyTextPanel, OutputPanel> e in panels) {
+        if (String.IsNullOrEmpty(e.Value.Screen.Error))
+            e.Key.WriteText(e.Value.Screen.Evaluate(cargo));   
+        else
+            e.Key.WriteText(e.Value.Screen.Error);    
+        // e.Key.WriteText(sb.ToString());
+    }
+
+}
+
+private void UpdatePanelConfig(OutputPanel p) {
+    string s = p.Panel.CustomData;
+    if (!s.StartsWith("!stik:lcd-info")) { // guess it's something else now
+        panels.Remove(p.Panel);
+        return; 
+    }
+    p.Config = s;
+    p.Screen = ScreenThing.Parse(GridTerminalSystem, DeleteFirstLine(s));
+
+}
+
+public static string DeleteFirstLine(string s) {
+    int i = s.IndexOf('\n') + 1;
+    return s.Substring(i);
+}
 
 //
 // Returns a Dictionary with all the cargo items in the grid
@@ -50,23 +104,49 @@ public Dictionary<string, int> GetAllCargo(IMyGridTerminalSystem gts) {
     //
     // Get a list of all containers in the grid
     //
-    List<IMyCargoContainer> containers = new List<IMyCargoContainer>();
-    gts.GetBlocksOfType<IMyCargoContainer>(containers);
+    List<IMyEntity> containers = new List<IMyEntity>();
+    gts.GetBlocksOfType<IMyEntity>(containers);
 
-    foreach (IMyCargoContainer container in containers) {
-        var items = container.GetInventory(0).GetItems();
-        foreach (var item in items) {
-            string k = item.Content.TypeId.ToString();
-            if (!res.ContainsKey(k)) 
-                res.Add(k, 0);
-            res[k] = res[k] + item.Amount;
+    foreach (IMyEntity container in containers) {
+        if (container.InventoryCount == 0)
+            continue;
+        for (int j = 0; j < container.InventoryCount; ++j) {
+            List<MyInventoryItem> items = new List<MyInventoryItem>();
+            container.GetInventory(j).GetItems(items, null);
+            foreach (var item in items) {
+                string k = StripClassName(item.Type.TypeId.ToString()) + "_" + item.Type.SubtypeId.ToString();
+                if (!res.ContainsKey(k)) 
+                    res.Add(k, 0);
+                res[k] = res[k] + item.Amount.ToIntSafe();
+            }
         }
     }  
 
     return res;
 }
 
+//
+// Remove MyObjectBuilder_ from the start of s, if present
+//
+private static string StripClassName(string s) {
+    if (s.StartsWith("MyObjectBuilder_"))
+        return s.Substring(16);
+    return s;
+}
 
+
+
+public class OutputPanel {
+    private IMyTextPanel panel;
+    public string Config;
+    public ScreenThing Screen;
+
+    public OutputPanel(IMyTextPanel panel) {
+        this.panel = panel;
+    }
+
+    public IMyTextPanel Panel { get { return panel; }}
+}
 
 
 private static string PadLeft(string s, int width) {
@@ -80,6 +160,8 @@ public class ScreenThing {
     private List<ScreenElement> elements = new List<ScreenElement>();
 
     public readonly IMyGridTerminalSystem gts;
+
+    public string Error { get { return error; }}
 
     private ScreenThing(IMyGridTerminalSystem gts) {
         this.gts = gts;
@@ -146,7 +228,6 @@ public class ScreenThing {
                 res.Add(input.Substring(last), true);
         } catch (Exception ex) {
             res.error = ex.ToString();
-            Console.WriteLine("Error: " + ex.ToString());
         }
         return res;
     }
@@ -187,9 +268,7 @@ public class ScreenElementMass : ScreenElement {
     }		
 
     public override string Get(Dictionary<string, int> storage) {
-        if (vPadWidth > 0)
-            return PadLeft("9872", vPadWidth);
-        IMyShipController c = owner.gts.GetBlockWithName("Cockpit 2") as IMyShipController;
+        IMyShipController c = owner.gts.GetBlockWithName(COCKPIT_NAME) as IMyShipController;
         int m = (int)(c.CalculateShipMass().TotalMass / 1000.0);
         string s = m.ToString();    
         if (vPadWidth <= 0)
@@ -216,7 +295,7 @@ public class ScreenElementItemCount : ScreenElement {
         int m;
         string s;
         if (!storage.TryGetValue(vResource, out m)) 
-            s = "???";
+            s = "-";
         else 
             s = m > 1000 ? (int)(m / 1000) + "K" : m.ToString();							
         if (vPadWidth <= 0)
